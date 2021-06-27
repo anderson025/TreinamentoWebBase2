659c857b5d5f7b28ea52859a7234837768029d54
Anderson Silva <anderson.silva@base2.com.br>
2021-06-27 14:21:22 -0300

    Finalizado a criação da classe crowtest

--- ProjetoNUnit/UnitTest1.cs
+++ ProjetoNUnit/UnitTest1.cs
@@ -97,12 +97,41 @@ namespace ProjetoNUnit
 
         }
         [Test]
+        [Obsolete]
         public void CrowdTest()
         {
-             string url = "http://blackmirror.crowdtest.me.s3-website-us-east-1.amazonaws.com/landing";
+            string url = "http://blackmirror.crowdtest.me.s3-website-us-east-1.amazonaws.com/landing";
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl(url);
 
+            driver.FindElement(By.CssSelector("a[class='cc-btn cc-dismiss']")).Click();
+
+            driver.FindElement(By.Id("login")).SendKeys("anderson.silva@base2.com.br");
+            driver.FindElement(By.Id("password")).SendKeys("Sua senha");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            //Selecionando o botão confirmar
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button[class='btn btn-crowdtest btn-block']"))); 
+            driver.FindElement(By.CssSelector("button[class='btn btn-crowdtest btn-block']")).Click();
+            //Selecionando o botão Geranciar
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("button[class='btn btn-crowdtest mr-1']"))); 
+            driver.FindElement(By.CssSelector("button[class='btn btn-crowdtest mr-1']")).Click();
+            //Selecionando o menu projeto
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div ul > li:nth-child(4)"))); 
+            driver.FindElement(By.CssSelector("div ul > li:nth-child(4)")).Click();
+            //Selecionando o projeto
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div mat-table mat-row mat-cell"))); 
+            driver.FindElement(By.CssSelector("div mat-table mat-row mat-cell")).Click();
+
+            //Acessando o menu Casos de Teste
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[text() ='Casos de Teste']"))); 
+            driver.FindElement(By.XPath("//div[text() ='Casos de Teste']")).Click();
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div/mat-table/mat-row/mat-cell[text() = '1']"))); 
+            string grid = driver.FindElement(By.XPath("//div/mat-table/mat-row/mat-cell[text() = '1']")).ToString();
+            
+            Assert.IsNotEmpty(grid);
             
         }
 
